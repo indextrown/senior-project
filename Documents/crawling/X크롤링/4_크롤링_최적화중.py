2719fe061e52fdeb11f910c5b538af3142ce44b9
indextrown <lghan00020@nate.com>
2024-05-27 16:16:27 +0900

    크롤링 광고제거 및 최적화 리팩토링중

---
+++ Documents/crawling/X크롤링/4_크롤링_최적화중.py
@@ -0,0 +1,189 @@
+import pickle
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
+from webdriver_manager.chrome import ChromeDriverManager
+from bs4 import BeautifulSoup
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+
+import time
+import sys
+
+
+ban_list = ["다현, 플라워샵", "정대만", "럭키드로우", "오레오"]
+# 전역 변수 선언
+driver = None
+
+# 봇 감지 전처리
+def open_Driver():
+
+    # 함수안에서 Local이 아닌 Global을 사용하겠다.
+    global driver
+    options = Options()
+
+    #options.add_experimental_option("detach", True)
+    #options.add_argument("--headless")  # 헤드리스 모드로 실행
+
+    # Windows 10 운영 체제에서 Chrome 브라우저를 사용하는 것처럼 보이는 사용자 에이전트가 설정
+    options.add_argument('user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36')
+    
+    # 언어 설정을 한국어로 지정. 이는 웹 페이지가 한국어로 표시
+    options.add_argument('lang=ko_KR')   
+    
+    # 브라우저 창의 크기를 지정. 여기서는 너비 430px, 높이 932px로 설정
+    options.add_argument('--window-size=932,932') 
+    
+    # GPU 가속을 비활성화. GPU 가속이 활성화되어 있으면, Chrome이 GPU를 사용하여 그래픽을 렌더링하려고 시도할 수 있기때문. 일부 환경에서는 GPU 가속이 문제를 일으킬 수 있으므로 이 옵션을 사용하여 비활성화
+    options.add_argument('--disable-gpu')
+
+    # 정보 표시줄을 비활성화. 정보 표시줄은 Chrome 브라우저 상단에 나타나는 알림이나 메시지를 의미. 이 옵션을 사용하여 이러한 알림이 나타나지 않도록 설정.
+    options.add_argument('--disable-infobars')
+
+    # 확장 프로그램을 비활성화. Chrome에서 확장 프로그램을 사용하지 않도록 설정
+    options.add_argument('--disable-extensions')
+
+    #  자동화된 기능을 비활성화. 이 옵션은 Chrome이 자동화된 환경에서 실행되는 것을 감지하는 것을 방지
+    options.add_argument('--disable-blink-features=AutomationControlled')
+
+    # 자동화를 비활성화. 이 옵션은 Chrome이 자동화 도구에 의해 제어되는 것으로 감지되는 것을 방지
+    options.add_argument('--disable-automation')
+
+    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
+
+# 사이트에 쿠키 적용
+def check_login_selenium_x():
+    global driver
+    # url 이동
+    url = 'https://www.twitter.com'
+    driver.get(url)
+    driver.implicitly_wait(15)
+
+    cookies = pickle.load(open('x_cookies.pkl', 'rb'))
+    for cookie in cookies:
+        driver.add_cookie(cookie)
+
+    driver.refresh()
+    driver.implicitly_wait(15)
+
+    cur_url = driver.current_url
+
+    
+    if (cur_url[-4:] == "home"):
+        #print("로그인 성공")
+        return True;
+    else:
+        #print("로그인 실패로 프로그램 종료합니다.")
+        sys.exit()
+        return False;
+
+def search_x(search):
+    global driver
+    driver.get(f"https://twitter.com/search?q={search}&src=typed_query&f=live")
+    driver.implicitly_wait(15)
+    time.sleep(3)
+
+    # old_titles = []
+    # ongoing = True 
+
+    # 스크롤을 조금씩 내리는 JavaScript 코드
+    scroll_script = """
+        var currentPosition = window.pageYOffset;
+        var targetPosition = currentPosition + 500;
+        var scrollInterval = setInterval(function() {
+            if (currentPosition >= targetPosition) {
+                clearInterval(scrollInterval);
+            } else {
+                window.scrollBy(0, 50);
+                currentPosition += 50;
+            }
+        }, 100);
+    """
+
+    titles = driver.find_elements(By.CSS_SELECTOR, "#react-root > div > div > div.css-175oi2r.r-1f2l425.r-13qz1uu.r-417010.r-18u37iz > main > div > div > div > div > div > div:nth-child(3) > section > div > div > div")
+
+    for title in titles:
+        #li = title.find_elements(By.CSS_SELECTOR, "div > div > article > div > div > div:nth-child(2) > div:nth-child(2) > div")
+        try:
+            li = title.find_elements(By.CSS_SELECTOR, "div > div > article > div > div > div:nth-child(2) > div:nth-child(2) > div")
+            info = li[0].text.split("\n")
+
+            # 광고게시물 무시
+            if info[-1] == 'Ad': 
+                print("광고게시물 무시합니다")
+                print("\n\n")
+                continue
+            
+            ban = False
+            for ban_keyword in ban_list:
+                # 특정 벤 리스트 키워드 무시
+                if (ban_keyword in li[1].text):
+                    #print(f"{ban_keyword} 가 게시물에 있기 때문에 해당 게시물 무시합니다.")
+                    ban = True
+
+            # 벤 키워드가 있는 게시물 무시
+            if (ban): 
+                print("벤게시글 무시합니다")
+                print("\n\n")
+                ban = False
+                continue
+            
+
+            #print(li[1].text, "###########")            
+            # 프로필 url
+            profile_url = li[0].find_element(By.CSS_SELECTOR, "div > div > div > div > div > div > a").get_attribute('href')
+            
+            # 게시글 url
+            title_url = li[0].find_element(By.CSS_SELECTOR, "div > div > div > div > div:nth-child(2) > div > div:nth-child(3) > a").get_attribute('href')
+            
+            
+            # 게시시간
+            mytesttime = li[0].find_element(By.CSS_SELECTOR, "div > div > div > div > div:nth-child(2) > div > div:nth-child(3) > a > time").text
+        except Exception as e:
+            print(e)
+            continue
+
+        # 닉네임
+        nickname = info[0]
+
+        # 유저id
+        user_id = info[1]
+
+        # 게시글
+        new_title = li[1].text
+        clear_new_title = new_title.split()
+        clear_new_title = ' '.join(clear_new_title)
+
+        print("닉네임: ",nickname)
+        print("작성자: ", user_id)
+        print("게시시간: ", mytesttime)
+        print("게시물내용\n", clear_new_title)
+        print("\n\n")
+
+
+
+# 크롤링 진행 
+def main():
+    global driver
+    try:
+        # 봇 감지 우회 전처리
+        open_Driver()
+        
+        # 로그인 성공하면 생일카페 검색 로직 시작
+        if (check_login_selenium_x()):
+            print("잔행 시작")
+            crawling_list = search_x("생일카페")
+            #input()
+
+    except Exception as e:
+        print(e)
+        print("err")
+    finally:
+        driver.quit()
+
+if __name__ == "__main__":
+    main()
+
+
+
+
