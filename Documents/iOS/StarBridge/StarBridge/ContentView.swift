6220426e5e7e19b58ae1850653c5b5cc18f66781
opficdev <opficdev@gmail.com>
2024-06-30 05:22:40 +0900

    iOS 앱 기초 내용 추가

---
+++ Documents/iOS/StarBridge/StarBridge/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  StarBridge
+//
+//  Created by 최윤진 on 2024-06-30.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Documents/iOS/StarBridge/StarBridge/StarBridgeApp.swift
@@ -0,0 +1,17 @@
+//
+//  StarBridgeApp.swift
+//  StarBridge
+//
+//  Created by 최윤진 on 2024-06-30.
+//
+
+import SwiftUI
+
+@main
+struct StarBridgeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
