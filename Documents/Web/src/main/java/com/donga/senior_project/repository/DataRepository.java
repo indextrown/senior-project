99af6814a62b2ecdaa31771e3e8797bf20222cf0
DoNalD-A <thdlstjrj@naver.com>
2024-07-19 22:23:09 +0900

    new web

--- Documents/Web/src/main/java/com/donga/senior_project/repository/DataRepository.java
+++
@@ -1,9 +0,0 @@
-package com.donga.senior_project.repository;
-
-import com.donga.senior_project.model.Data;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface DataRepository extends JpaRepository<Data, Integer> {
-}
---
+++ Documents/Web/src/test/java/com/starbridge/senior_project/SeniorProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.starbridge.senior_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeniorProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
