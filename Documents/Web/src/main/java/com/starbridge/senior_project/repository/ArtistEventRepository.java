4a2c37568901419fcb63667bb5711efa681932d1
DoNalD-A <thdlstjrj@naver.com>
2024-08-20 20:54:45 +0900

    불필요한 파일 삭제

--- Documents/Web/src/main/java/com/starbridge/senior_project/repository/ArtistEventRepository.java
+++
@@ -1,9 +0,0 @@
-//package com.starbridge.senior_project.repository;
-//
-//import com.starbridge.senior_project.model.ArtistEvent;
-//import org.springframework.data.jpa.repository.JpaRepository;
-//import org.springframework.stereotype.Repository;
-//
-//@Repository
-//public interface ArtistEventRepository extends JpaRepository<ArtistEvent, Integer> {
-//}
